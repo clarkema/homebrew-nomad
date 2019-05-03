812d291a5d8c6b48be076d47de9a4cbd3da7b1da
Mike Clarke <mike@lambdafunctions.com>
2019-05-03 13:16:38 +0100

    x12pp: Update to 0.3.0

--- x12pp.rb
+++ x12pp.rb
@@ -1,8 +1,8 @@
 class X12pp < Formula
   desc "X12 pretty printer"
   homepage "https://github.com/clarkema/x12pp"
-  url "https://github.com/clarkema/x12pp/archive/v0.2.0.tar.gz"
-  sha256 "d1c2c0be9787822c2b26047e8da2d4001775acaf021bbe1189b2ef427933a695"
+  url "https://github.com/clarkema/x12pp/archive/v0.3.0.tar.gz"
+  sha256 "96eae45a2e516833b2d16007dd97391100430848bffea92c39fff66b1cb7ce18"
   head "https://github.com/clarkema/x12pp.git"
 
   depends_on "rust" => :build
